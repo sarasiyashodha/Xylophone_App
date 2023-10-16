75d56282e96a21c8d90935d4878670d068712693
Sarasi Yashodha <sarasiyashodha67@gmail.com>
2023-10-16 23:33:28 +0530

    Removed debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
           child: Column(
